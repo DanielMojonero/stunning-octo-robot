d5560aa9c7d635a9e1092a976c88352acd1d13bf
DanielMojonero <danielmm95@gmail.com>
2016-05-19 12:32:38 +0200

    Primera prueba

--- src/stunningoctorobot/StunningOctoRobot.java
+++ src/stunningoctorobot/StunningOctoRobot.java
@@ -5,7 +5,6 @@ public class StunningOctoRobot {
     public static void main(String[] args) {
         System.out.println("stunning-octo-robot");
         System.out.println("stunning-octo-robot2222");
-           System.out.println("stunning-octo-robot222244");
     }
     
 }
